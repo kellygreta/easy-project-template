f6fd249d11c6c8c685bb4d14c6f5e4dd91ed719f
kellygreta <greta.vigano97@gmail.com>
2025-09-18 17:28:37 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Layout from "./components/Layout";
 import ExampleModule from "./modules/ExampleModule";
+import HeroSection from "./modules/HeroSection";
+import AboutSection from "./modules/AboutSection";
+import ServicesSection from "./modules/ServicesSection";
+import GallerySection from "./modules/GallerySection";
 
 export default function App() {
   return (
     <Layout>
-      <ExampleModule />
+      <HeroSection />
+      <AboutSection />
+
+      <ServicesSection />
+      <GallerySection />
+      {/* <ExampleModule /> */}
     </Layout>
   );
 }
